60ec373cc9e3a28b17d94cf4fd2e164bc39357e0
ahrobinson <ahrobins3@gmail.com>
2016-05-22 20:36:31 -0500

    media template will call nuviData method on creation, add getNuviData helper

--- client/media/media.js
+++ client/media/media.js
@@ -1,9 +1,19 @@
 Template.media.onCreated(function mediaOnCreated() {
-
+  Meteor.call("nuviData", function(error, result){
+    if(error){
+      console.log("error", error);
+    }
+    if(result){
+      console.log(result);
+      Session.set('nuviData', result);
+    }
+  });
 });
 
 Template.media.helpers({
-
+  getNuviData() {
+    return Session.get('nuviData');
+  },
 });
 
 Template.media.events({
